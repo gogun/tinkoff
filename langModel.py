e3e9fb2e71b1590ceace5fe067cff02db6e2ad67
ilya <gusarov2000@gmail.com>
2019-09-05 01:16:43 +0300

    и последний коммит после дедлайна(я надеюсь), обернул в ООП, также можете не смотреть

--- langModel.py
+++ langModel.py
@@ -1,30 +1,32 @@
 import random
+class Model():
 
-def fit(input):
-    input = input.split(' ')
-    for i in range(len(input)-3):
-        data.append(input[i:i+2])
+    data = [] #массив с биграммной языковой моделью 
+    
+    def fit(self, input):
+        string = "" #обработанный текст
+        for char in input:
+            if ((char != '.') and (char != ',') and (char != '!') and (char != '?') and (char != '-')):
+                string += char
+        string = string.lower()
+        
+        string = string.split(' ')
+        for i in range(len(string)-3):
+            Model.data.append(string[i:i+2])
 
-def generation(n):
-    for _ in range(n):
-        tmp = random.choice(data)
-        print(tmp[0], tmp[1], end = ' ')
-        for j in range(len(data)):
-            if tmp[1] == data[j][0]:
-                print(data[j][1], end = ' ')
-                break
-    print()
+    def generation(self, n):
+        for _ in range(n):
+            tmp = random.choice(Model.data)
+            print(tmp[0], tmp[1], end = ' ')
+            for j in range(len(Model.data)):
+                if tmp[1] == Model.data[j][0]:
+                    print(Model.data[j][1], end = ' ')
+                    break
+        print()
 
-  
 g = int(input()) #длина сгенерированной строки (результат будет в 3 раза длиннее желаемого (фича))
-
-data = []
-string = ""
 f = open('Tolstoy.txt')
-for char in f.read():
-    if ((char != '.') and (char != ',') and (char != '!') and (char != '?') and (char != '-')):
-        string += char
-string = string.lower()
 
-fit(string)
-generation(g)
+lang_model = Model()
+lang_model.fit(f.read())
+lang_model.generation(g)
